500dd73403e02993eac1a3a35392089c884ad855
pablo <pablovagez@hotmail.com>
2021-10-06 11:03:47 +0200

    Init

---
+++ pages/posts/first-post.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from "next/image"
+import Link from "next/link"
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+    return (
+<Layout>
+        <Head>
+            <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+            <Link href="/">
+                <a>Back to home</a>
+            </Link>
+        </h2>
+        <Image src="/images/profile.jpg" height={144} width={144} alt="Profile picture">
+        </Image>
+</Layout>
+    )
+}
